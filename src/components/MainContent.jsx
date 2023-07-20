64442e00a0e2497064aece6a156e3a33264827af
Pancho Venes <francisco.venes@gmail.com>
2023-07-20 14:54:05 +0100

    minor corrections

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,7 @@ function MainContent() {
       <h2 className="info--title">About</h2>
       <p className="info--paragraph">
         I am a PhD candidate in Sociology who is also interested in Frontend
-        Development, particularly Rect. I started programming to develop
+        Development, particularly React. I started programming to develop
         personal projects. I also built a full-stack app and want to know more
         about Node.js and authentication.
       </p>
